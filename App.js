e7051eb4a68e9d36b1a5dc91f69819217128192d
shouravbr <shouravbr@outlook.com>
2022-02-04 02:37:12 +0600

    bottom nav added

--- App.js
+++ App.js
@@ -1,24 +1,76 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { AntDesign } from '@expo/vector-icons';
 
-function HomeScreen() {
+const SingleQuote = (props) => {
+  return(
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>{props.text}--{props.author}</Text>
+    </View>
+  )
+}
+
+function Quotes() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Quotes in the Wild!</Text>
+      <SingleQuote text="Old is Gold" author="Golden Girls"/>
     </View>
   );
 }
 
-const Stack = createNativeStackNavigator();
+function Favourites() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Your Favourites!</Text>
+    </View>
+  );
+}
+
+function Write() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Write your own quote!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Quotes" component={Quotes}
+        options={{
+          tabBarLabel: 'Quotes',
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="book" color={color} size={size} />
+          ),
+        }} />
+      <Tab.Screen name="Favourites" component={Favourites}
+        options={{
+          tabBarLabel: 'Favourites',
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="heart" color={color} size={size} />
+          ),
+        }} />
+      <Tab.Screen name="Write" component={Write}
+        options={{
+          tabBarLabel: 'Write',
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="form" color={color} size={size} />
+          ),
+        }} />
+    </Tab.Navigator>
+  );
+}
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
+      <MyTabs />
     </NavigationContainer>
   );
 }
